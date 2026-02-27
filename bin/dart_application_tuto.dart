fa7e23defad2993c06244b381a478fe9fbf12b88
Olivier Pleynet <olivier.pleynet@atos.net>
2026-02-27 18:08:02 +0100

    Add search params

--- bin/dart_application_tuto.dart
+++ bin/dart_application_tuto.dart
@@ -1,9 +1,26 @@
 const version = '0.0.1';
 
 void main(List<String> arguments) {
-  if (arguments.isEmpty) {
-    print('Hello, Dart!');
+  if (arguments.isEmpty || arguments.first == 'help') {
+    printUsage();
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
+  } else if (arguments.first == 'search') {
+    // Add this new block:
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
+  } else {
+    printUsage();
   }
 }
+
+void searchWikipedia(List<String>? arguments) { // Add this new function and add ? to arguments type
+  print('searchWikipedia received arguments: $arguments');
+}
+
+void printUsage() { // Add this new function
+  print(
+    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'"
+  );
+}
+
